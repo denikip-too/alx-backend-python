e2a8c5f730a30685dccbd8b1058c4d3feb3ccd7a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-26 13:11:05 -0700

    fix

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,13 +4,8 @@ from typing import Mapping, Any, Union, TypeVar
 T = TypeVar('T')
 
 
-"""define NoneType"""
-NoneType = type(None)
-"""define NoneType"""
-
-
 def safely_get_value(
-        dct: Mapping, key: Any, default: Union[T, NoneType]
+        dct: Mapping, key: Any, default: Union[T, None]
         ) -> Union[Any, T]:
     """Given the parameters and the return values,
     add type annotations to the function"""
