aa429bb555e5bba4daaf2a9b13ffe6cd365c67a2
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-06-27 16:58:26 -0700

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""execute multiple coroutines at the same time with async"""
+import asyncio
+import random
+
+
+async def task_wait_n(n: int, max_delay: int) -> float:
+    """execute multiple coroutines at the same time with async"""
+    res = []
+    temp = []
+    for i in range(n):
+        max_delay = random.uniform(0, max_delay)
+        res.append(max_delay)
+    while res:
+        minimum = res[0]
+        for x in res:
+            if x < minimum:
+                minimum = x
+        temp.append(minimum)
+        res.remove(minimum)
+    return (temp)
