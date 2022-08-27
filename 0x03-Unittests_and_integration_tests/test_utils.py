038c4fa82cf93a144837d93a4e41457b42f6c31c
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-27 21:06:35 +0000

    ...

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Parameterize a unit test"""
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """subclass"""
+
+    @parameterized.expand([
+        [{"a": 1}, ("a",)],
+        [{"a": {"b": 2}}, ("a",)],
+        [{"a": {"b": 2}}, ("a", "b")]
+        ])
+
+    def test_access_nested_map(self, a, b):
+        """test that the method returns what it is supposed to"""
+        self.assertEqual(a, b)
