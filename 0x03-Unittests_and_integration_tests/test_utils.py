ee886a2450f47f2162e5f4684958ecadd0fb17b6
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-28 13:38:36 +0000

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,16 +2,17 @@
 """Parameterize a unit test"""
 import unittest
 from parameterized import parameterized
+from utils import (access_nested_map, get_json, memoize)
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """subclass"""
 
     @parameterized.expand([
-        [{"a": 1}, ("a",)],
-        [{"a": {"b": 2}}, ("a",)],
-        [{"a": {"b": 2}}, ("a", "b")]
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self, a, b):
+    def test_access_nested_map(self, nested_map, path, expected):
         """test that the method returns what it is supposed to"""
-        self.assertEqual(a, b)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
