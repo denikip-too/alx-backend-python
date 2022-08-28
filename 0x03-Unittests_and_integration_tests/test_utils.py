7d9d53eb293447b2600bbbb8be52495b8acf0d4b
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-28 19:31:40 +0000

    add

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,10 +9,20 @@ class TestAccessNestedMap(unittest.TestCase):
     """subclass"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",)),
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map, path, expected):
         """test that the method returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+        ])
+    def test_access_nested_map_exception(nested_map, path, expected):
+        """test that KeyError is raised using assertRaises context manager"""
+        with self.assertRaises(KeyError) as raises:
+            self.assertEqua(access_nested_map(nested_map, path), expected)
+            raise
