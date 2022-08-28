f76b3ee782d5f7c78e2ea00207786904d9a08b32
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-28 20:21:42 +0000

    add

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,10 +3,12 @@
 import unittest
 from parameterized import parameterized
 from utils import (access_nested_map, get_json, memoize)
+import requests
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """subclass"""
+    """class TestAccessNestedMap"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -26,3 +28,12 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as raises:
             self.assertEqua(access_nested_map(nested_map, path), expected)
             raise
+
+class TestGetJson(unittest.TestCase):
+    """class TestGetJson"""
+
+    @patch('http://example.com', {"payload": True})
+    @patch('http://holberton.io', {"payload": False})
+    def test_get_json(self, url, payload):
+        """test that utils.get_json returns the expected result"""
+        self.assertEqual(get_json(url), payload)
