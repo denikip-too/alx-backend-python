d515b030d0dbba5b0c5a3c36f15911339b77637a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-29 00:27:08 +0000

    ...

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,7 +43,9 @@ class TestGetJson(unittest.TestCase):
 class TestMemoize(unittest.TestCase):
     """class TestMemoize"""
 
-    def test_memoize(self):
+    @patch('test_memoize.TestClass.a_method', 1)
+    @patch('test_memoize.TestClass.a_property', 2)
+    def test_memoize(self, call):
         """test_memoize method"""
         class TestClass:
             """class TestClass"""
@@ -56,3 +58,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 """a_property method"""
                 return (self.a_method())
+
+        self.assert_called_once(a_method(), 42)
+        self.assertEqual(a_property(), 42)
+        self.assertEqual(call.call_count, 2)
